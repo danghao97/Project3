b5cf543b64d487c3e59599f8e8ec243fa1b8204d
Nguyễn Đăng Hào <hao.nguyendang.97@gmail.com>
2018-04-23 01:04:40 +0700

    ten_doi_tuong

--- resources/views/Layouts/QLHT/Video/DanhSach.blade.php
+++ resources/views/Layouts/QLHT/Video/DanhSach.blade.php
@@ -14,7 +14,7 @@
                         <div class="row">
                             <div class="col-md-4">
                                 <label>Mã số:</label> {{$video->id_video}}<br>
-                                <label>Đối tượng:</label> {{$video->DoiTuong->ten}}<br>
+                                <label>Đối tượng:</label> {{$video->DoiTuong->ten_doi_tuong}}<br>
                                 <label>Tên video:</label> {{$video->ten_video}}<br>
                                 <a class="btn btn-primary" href="QLHT/XoaVD/{{$video->id_video}}" role="button">Xóa</a>
                             </div>
