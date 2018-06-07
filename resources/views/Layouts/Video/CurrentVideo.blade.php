d9c5da1947effaf9fb1932b7728a71232ea8afd5
Nguyễn Đăng Hào <37840906+danghao97@users.noreply.github.com>
2018-06-07 23:05:09 +0700

    Video controls

--- resources/views/Layouts/Video/CurrentVideo.blade.php
+++ resources/views/Layouts/Video/CurrentVideo.blade.php
@@ -4,14 +4,20 @@
     </div>
     <div class="panel-body">
         <div class="embed-responsive embed-responsive-16by9">
-            <video id="current" class="embed-responsive-item" src="http://{{$_SERVER['SERVER_NAME']}}:{{$NodeJS_Port}}/video/{{$video->id_video}}"></video>
+            <video controls id="current" class="embed-responsive-item" src="http://{{$_SERVER['SERVER_NAME']}}:{{$NodeJS_Port}}/video/{{$video->id_video}}"></video>
         </div>
-        <br><button type="button" name="button" onclick="playPause()">Play/Pause</button>
-        <button type="button" name="button" onclick="fullScreen()">Full Screen</button>
         <p>Tên video: {{$video->ten_video}}</p>
         <p>Thời lượng quay: {{$video->thoi_gian}}</p>
         <p>Kich thuoc: {{$video->kich_thuoc}}</p>
         <button type="button">Sửa thông tin video</button>
         <button type="button">Xóa video</button>
+        <select title="Select your surfboard" class="selectpicker">
+            <option>Select...</option>
+            <option data-thumbnail="http://{{$_SERVER['SERVER_NAME']}}:{{$NodeJS_Port}}/image">Chrome</option>
+            <option data-thumbnail="http://{{$_SERVER['SERVER_NAME']}}:{{$NodeJS_Port}}/image">Firefox</option>
+            <option data-thumbnail="http://{{$_SERVER['SERVER_NAME']}}:{{$NodeJS_Port}}/image">IE</option>
+            <option data-thumbnail="http://{{$_SERVER['SERVER_NAME']}}:{{$NodeJS_Port}}/image">Opera</option>
+            <option data-thumbnail="http://{{$_SERVER['SERVER_NAME']}}:{{$NodeJS_Port}}/image">Safari</option>
+        </select>
     </div>
 </div>
